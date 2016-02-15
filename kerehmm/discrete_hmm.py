a022be83b637f3a5bb0ffeb99da85d63ca3d8eb9
Kerem Eryilmaz <keryilmaz@gmail.com>
2016-02-15 21:47:11 +0100

    Finished implementing discrete HMM. Closes #1.

--- kerehmm/discrete_hmm.py
+++ kerehmm/discrete_hmm.py
@@ -62,40 +62,5 @@ class DiscreteHMM(AbstractHMM):
                 print "Row sum:", np.exp(np.logaddexp.reduce(trans_new[i]))
 
             print "New transition matrix:", np.exp(trans_new)
-            # print "Sums", np.sum.reduce(np.exp(trans_new), axis=1)
-            # # emission probabilities
-            # denominator = np.logaddexp.reduce(gamma)
-            # for state, distribution in enumerate(self.emissionDistributions):
-            #     state_map = np.array([np.log(1) if o == state else -np.inf for o in observations])
-            #     # print "Map:", np.atleast_2d(state_map).T
-            #     mapped_gamma = gamma + np.atleast_2d(state_map).T
-            #     # print "Mapped gamma:", mapped_gamma
-
-            # def do_pass():
-            #     # initial probabilities
-            #     pi_new = np.zeros_like(self.initialProbabilities)
-            #     xi = self.xi(observations=observations)
-            #     gamma = self.gamma2(observations=observations)
-            #     pi_new[:] = gamma[0]
-            #     print "New initial probs:", np.exp(pi_new)
-            #
-            #     # transition matrix
-            #     trans_new = np.empty_like(self.transitionMatrix)
-            #     trans_new[:] = -np.inf
-            #     for i, _ in enumerate(self.transitionMatrix):
-            #         for j, _ in enumerate(self.transitionMatrix[i]):
-            #             T = len(observations)
-            #             trans_new[i, j] = np.logaddexp.reduce(xi[:T - 1, i, j], axis=0) - \
-            #                               np.logaddexp.reduce(gamma[:T - 1, i], axis=0)
-
-            # print "New transition matrix:", np.exp(trans_new)
-            # emission probabilities
-            # denominator = np.logaddexp.reduce(gamma)
-            # for state, distribution in enumerate(self.emissionDistributions):
-            #     state_map = np.array([np.log(1) if o == state else -np.inf for o in observations])
-            #     # print "Map:", np.atleast_2d(state_map).T
-            #     mapped_gamma = gamma + np.atleast_2d(state_map).T
-            #     # print "Mapped gamma:", mapped_gamma
-
         for i in range(iterations):
             do_pass()
